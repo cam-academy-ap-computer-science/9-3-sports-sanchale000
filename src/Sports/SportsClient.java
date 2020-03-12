6847ed86024abe0959686980ee314cc0274b0bcd
sanchale000 <sanchale000@CAM-LAB-16.bgsd.k12.wa.us>
2020-03-12 07:55:57 -0700

    barry

--- src/Sports/SportsClient.java
+++ src/Sports/SportsClient.java
@@ -5,6 +5,9 @@ public class SportsClient {
 	public static void main(String[] args) {
 		Player bob = new Player("bob", 80, 48);
 		System.out.println(bob.toString());
+		BasketBallPlayer barry = new BasketBallPlayer("Barry", 35, 80, "Morons", 100000.0);
+		System.out.println(barry.toString());
+		
 	}
 
 }
